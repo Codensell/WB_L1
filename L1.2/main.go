99a75bcc901c93c463b0849dde1ac0697d0ecab1
Codensell <sadikov91@gmail.com>
2025-09-10 18:22:45 +0500

    fix: main.go добавил горутины, теперь числа из массива возводятся в квадрат

--- L1.2/main.go
+++ L1.2/main.go
@@ -1,18 +1,30 @@
 package main
 
-import(
+import (
 	"fmt"
+	"sync"
 )
 
-func main(){
-	array := []int{2, 4, 6, 8, 10}
-	result := multiply(array)
+func main() {
+	var wg sync.WaitGroup
 
-	fmt.Println(result)
-}
-func multiply(n []int) []int{
-	for i, elem := range n{
-		n[i] = elem * elem
+	array := []int{2, 4, 6, 8, 10}
+	ch := make(chan int)
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	for _, numArray := range array {
+		wg.Add(1)
+		go func(elem int) {
+			defer wg.Done()
+			ch <- elem * elem
+		}(numArray)
 	}
-	return n
+
+	for sqr := range ch {
+		fmt.Println(sqr)
+	}
 }
